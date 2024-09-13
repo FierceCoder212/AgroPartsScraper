dbd5515f976163cca00e6a9c0a99a36da9c78b9c
AbdullahFarooq123 <abdullahfarooq20172018@gmail.com>
2024-09-13 17:00:54 +0500

    Minor fix

--- Main.py
+++ Main.py
@@ -2,7 +2,9 @@ import json
 
 from Helpers.ScraperHelper import ScraperHelper
 from Models.ScraperInputModel import ScraperInputModel
+from dotenv import load_dotenv, find_dotenv
 
+load_dotenv(find_dotenv('.env'))
 with open('ModelData.json', 'r') as model_file:
     data = [ScraperInputModel(**item) for item in json.load(model_file)]
 scraper_helper = ScraperHelper(data)
